cb319191f9f94dfaf43888dd7223e59cf555117d
Gian Piero Canevari <canevarigian@gmail.com>
2025-07-23 22:11:01 +0200

    Min Stack Solved 2

--- Neetcode/medium/minimumstack.js
+++ Neetcode/medium/minimumstack.js
@@ -1,32 +1,61 @@
 class MinStack {
+    // constructor() {
+    //     this.stack = []
+    // }
+
+    // push(val) {
+    //     this.stack.push(val);
+    // }
+
+    // pop() {
+    //     this.stack.pop();
+    // }
+    
+    // top() {
+    //     return this.stack[this.stack.length - 1]
+    // }
+
+    // getMin() {
+    //     const temp = []
+    //     mini = this.stack[this.stack.length - 1]
+
+    //     while (this.stack.length > 0) {
+    //         mini = Math.min(mini, this.stack[this.stack.length - 1])
+    //         temp.push(this.stack.pop())
+    //     }
+
+    //     while (temp.length > 0) {
+    //         this.stack.push(temp.pop())
+    //     }
+    //     return mini
+    // }
+
     constructor() {
         this.stack = []
+        this.minStack = []
     }
 
     push(val) {
-        this.stack.push(val);
+        this.stack.push(val)
+        val = Math.min(
+            val, 
+            this.minStack.length === 0
+                ? val 
+                : this.minStack[this.minStack.length - 1]
+            );
+        this.minStack.push(val)
     }
 
     pop() {
-        this.stack.pop();
+        this.stack.pop()
+        this.minStack.pop()
     }
-    
-    top() {
+
+    top () {
         return this.stack[this.stack.length - 1]
     }
 
     getMin() {
-        const temp = []
-        mini = this.stack[this.stack.length - 1]
-
-        while (this.stack.length > 0) {
-            mini = Math.min(mini, this.stack[this.stack.length - 1])
-            temp.push(this.stack.pop())
-        }
-
-        while (temp.length > 0) {
-            this.stack.push(temp.pop())
-        }
-        return mini
+        return this.minStack[this.minStack.length - 1]
     }
 }

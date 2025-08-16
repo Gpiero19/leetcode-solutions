6b56ebbd016b48994bcd3d1eaaac907eb0289693
Gian Piero Canevari <canevarigian@gmail.com>
2025-08-16 18:05:08 +0200

    tpo K elements Redid

--- Neetcode/medium/topkelemets.js
+++ Neetcode/medium/topkelemets.js
@@ -1,21 +1,37 @@
-function topKFreq (nums, k) {
-    const count = {}
-    const freq = Array.from({ length: nums.length + 1}, () => [])
+// function topKFreq (nums, k) {
+//     const count = {}
+//     const freq = Array.from({ length: nums.length + 1}, () => [])
 
-    for (const n of nums) {
-        count[n] =  (count[n] || 0) + 1
-    }
-    for (const n in count) {
-        freq[count[n]].push(parseInt(n))
+//     for (const n of nums) {
+//         count[n] =  (count[n] || 0) + 1
+//     }
+//     for (const n in count) {
+//         freq[count[n]].push(parseInt(n))
+//     }
+
+//     const res = []
+//     for (let i = freq.length - 1; i > 0; i --) {
+//         for (const n of freq[i]) {
+//             res.push(n);
+//             if (res.length === k) {
+//                 return res
+//             }
+//         }
+//     }
+// }
+
+
+function topKFrequent(nums, k) {
+    const count = {};
+    for (const num of nums) {
+        count[num] = (count[num] || 0) + 1;
     }
 
-    const res = []
-    for (let i = freq.length - 1; i > 0; i --) {
-        for (const n of freq[i]) {
-            res.push(n);
-            if (res.length === k) {
-                return res
-            }
-        }
-    }
+    const arr = Object.entries(count).map(([num, freq]) => [
+        freq,
+        parseInt(num),
+    ]);
+    arr.sort((a, b) => b[0] - a[0]);
+
+    return arr.slice(0, k).map((pair) => pair[1]);
 }

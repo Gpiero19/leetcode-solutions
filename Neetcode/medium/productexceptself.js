8edb11b27a7aaf6e38e98cbff4c7581e30b4c531
Gian Piero Canevari <canevarigian@gmail.com>
2025-08-22 17:15:06 +0200

    Product Except Self

---
+++ Neetcode/medium/productexceptself.js
@@ -0,0 +1,15 @@
+function productExceptSelf(nums) {
+    const n = nums.length;
+    const res = new Array(n);
+
+    for (let i = 0; i < n; i++) {
+        let prod = 1;
+        for (let j = 0; j < n; j++) {
+            if (i !== j) {
+                prod *= nums[j];
+            }
+        }
+        res[i] = prod;
+    }
+    return res;
+}

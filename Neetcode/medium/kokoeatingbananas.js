6b9841828e2aaeb4b01066c4645597e329b2ab9f
Gian Piero Canevari <canevarigian@gmail.com>
2025-09-02 19:15:59 +0200

    kokos eating bananas Redid

--- Neetcode/medium/kokoeatingbananas.js
+++ Neetcode/medium/kokoeatingbananas.js
@@ -1,16 +1,41 @@
+// Brute Force approach
+// function minEatingSpeed(piles, h) {
+//     let speed = 1;
+//     while (true) {
+//         let totalTime = 0;
+//         for (let pile of piles) {
+//             totalTime += Math.ceil(pile / speed);
+//         }
+
+//         if (totalTime <= h) {
+//             return speed;
+//         }
+//         speed++;
+//     }
+// }
+
+
+// Binary search approach
 function minEatingSpeed(piles, h) {
-    let speed = 1;
-    while (true) {
+    let left = 1;
+    let right = Math.max(...piles);
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
         let totalTime = 0;
+
         for (let pile of piles) {
-            totalTime += Math.ceil(pile / speed);
+            totalTime += Math.ceil(pile / mid);
         }
 
         if (totalTime <= h) {
-            return speed;
+            right = mid;
+        } else {
+            left = mid + 1;
         }
-        speed++;
     }
+
+    return left;
 }
 
-console.log(minEatingSpeed([1, 4, 3, 2], 9))
+console.log(minEatingSpeed([1, 4, 3, 2], 9)) 
